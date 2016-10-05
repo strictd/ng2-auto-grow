d1fc8656a68be01f8a2bb61db7b887fea6df8d3b
Gary <gary@strictdevelopment.com>
2016-10-04 21:51:10 -0700

    removed added correction from contentcheck

--- src/auto-grow.ts
+++ src/auto-grow.ts
@@ -12,18 +12,12 @@ export class AutoGrowDirective {
       el.nativeElement.style.height = ((el.nativeElement.scrollHeight - correction) + 10) + 'px';
     }, 0);
   }
-  /*@HostListener('input', ['$event.target']) onMouseEnter(textArea: any) {
-    textArea.style.height = 'auto';
-
-    let correction = textArea.offsetHeight - textArea.clientHeight;
-    textArea.style.height = ((textArea.scrollHeight - correction) + 10) + 'px';
-  }*/
 
   ngAfterContentChecked() {
     this.el.nativeElement.style.height = 'auto';
     
     let correction = this.el.nativeElement.offsetHeight - this.el.nativeElement.clientHeight;
-    this.el.nativeElement.style.height = ((this.el.nativeElement.scrollHeight - correction) + 10) + 'px';
+    this.el.nativeElement.style.height = ((this.el.nativeElement.scrollHeight - correction)) + 'px';
   }
 
 }
