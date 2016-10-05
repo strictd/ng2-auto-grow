76ca3d94ed82d5eb511cbe0e80a7c07366e06cf5
Gary <gary@strictdevelopment.com>
2016-10-04 21:28:24 -0700

    changed to ngAfterContentChecked

--- src/auto-grow.ts
+++ src/auto-grow.ts
@@ -12,11 +12,18 @@ export class AutoGrowDirective {
       el.nativeElement.style.height = ((el.nativeElement.scrollHeight - correction) + 10) + 'px';
     }, 0);
   }
-  @HostListener('input', ['$event.target']) onMouseEnter(textArea: any) {
+  /*@HostListener('input', ['$event.target']) onMouseEnter(textArea: any) {
     textArea.style.height = 'auto';
 
     let correction = textArea.offsetHeight - textArea.clientHeight;
     textArea.style.height = ((textArea.scrollHeight - correction) + 10) + 'px';
+  }*/
+
+  ngAfterContentChecked() {
+    this.el.nativeElement.style.height = 'auto';
+    
+    let correction = this.el.nativeElement.offsetHeight - this.el.nativeElement.clientHeight;
+    this.el.nativeElement.style.height = ((this.el.nativeElement.scrollHeight - correction) + 10) + 'px';
   }
 
 }
